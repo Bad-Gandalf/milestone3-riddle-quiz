78a97cb995905ba58dcc3d5228b129d4fad9f815
Patrick Doherty <patrick.e.doherty@gmail.com>
2018-04-10 20:39:17 +0000

    Scoring system corrected to not cutoff after 10th Q. Display correct question number, reset username and score when a new user logs in

--- run.py
+++ run.py
@@ -1,35 +1,104 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
-@app.route('/')
-def index():
+user = {"username":"",
+        "score" : 0 }
+
+def write_to_file(filename, data):
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+def add_to_scoreboard(username, score):
+    write_to_file('data/scoreboard.txt', "{0} {1} {2}\n".format(
+            score,
+            username.title(),
+            datetime.now().strftime("%d/%m/%y")))
+            
+def display_scoreboard():
+    scoreboard = []
+    details =[]
+    with open('data/scoreboard.txt', 'r') as score_data:
+        scoreboard = score_data.readlines()
+        ordered = sorted(scoreboard, key= (lambda line: int(line.lstrip().split(' ')[0])))
+        for score in ordered[::-1]:
+            details.append(score.split())
+        
+    return details
+    
+
+    
+
+@app.route('/', methods = ["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        user['username'] = username
+        user['score'] = 0
+        return redirect('/1')
     return render_template("index.html")
     
-@app.route('/leaderboard')
-def leaderboard():
-    return render_template("leaderboard.html")
-    
-@app.route('/riddles')
-def riddles():
-    return render_template("riddles.html")
-    
-@app.route('/riddles/<number>')
-def riddle_number(number):
-    member = {}
-    
+
+@app.route('/<number>', methods=["GET", "POST"])
+def get_info(number):
+    riddle = {}
     with open("data/riddle_data.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == number:
-                member = obj
-                
-        return render_template("riddles.html", member=riddle_url)
+                riddle = obj
+                total = str(int(obj['url']) - 1)
+        
+  
+    return render_template("member.html", riddle=riddle, user=user, total=total)
+
+
+@app.route('/submit_answer', methods = ["POST"])
+def check_answer():
+    if request.method == 'POST':
+        url = int(request.form["url"])
+        guess = request.form['answer'].title()
+        answer = request.form["solution"]
+        if url < 10:
+            if guess == answer:
+                url += 1
+                user['score'] += 1
+                return redirect(url)
+            elif guess == "Pass":
+                url += 1
+                return redirect(url)
+            else:
+                flash('"{}" is incorrect. Please try again'.format(guess))
+                return redirect(url)
+        else:
+            if guess == answer:
+                user['score'] += 1
+                add_to_scoreboard(user['username'], user['score'])
+                return redirect('leaderboard')
+            elif guess == "Pass":
+                url += 1
+                add_to_scoreboard(user['username'], user['score'])
+                return redirect('leaderboard')
+            else:
+                flash('"{}" is incorrect. Please try again'.format(guess))
+                return redirect(url)
+            
+            
+            
+ 
+@app.route('/leaderboard')
+def leaderboard():
+    scoreboard = display_scoreboard()
+    
+    return render_template("leaderboard.html", scoreboard=scoreboard, user=user)
+    
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug = True)
+            debug = True)  
