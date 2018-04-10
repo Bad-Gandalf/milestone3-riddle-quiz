65fd633d9df9d90b11d4cafe6a6bae801cb7384b
Patrick Doherty <patrick.e.doherty@gmail.com>
2018-04-10 14:47:53 +0000

    functional quiz, without score, leaderboard and flash messages

---
+++ try.py
@@ -0,0 +1,57 @@
+import os
+import json
+from flask import Flask, render_template, request, redirect, flash, url_for
+from datetime import datetime
+
+app = Flask(__name__)
+
+user = {}
+
+@app.route('/', methods = ["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        user['username'] = username
+        return redirect('/1')
+    return render_template("index.html")
+    
+
+@app.route('/<number>', methods=["GET", "POST"])
+def get_info(number):
+    riddle = {}
+    with open("data/riddle_data.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == number:
+                riddle = obj
+  
+    return render_template("member.html", riddle=riddle )
+
+
+@app.route('/submit_answer', methods = ["POST"])
+def check_answer():
+    if request.method == 'POST':
+        url = int(request.form["url"])
+        guess = request.form['answer'].title()
+        answer = request.form["solution"]
+        if url < 10:
+            if guess == answer:
+                url += 1
+                return redirect(url)
+            elif guess == "Pass":
+                url += 1
+                return redirect(url)
+            else:
+                return redirect(url)
+        else:
+            return redirect('leaderboard')
+            
+ 
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug = True)   
